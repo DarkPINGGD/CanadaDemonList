56c6cc2e10c30ade50d02b7d74f2939e7b75aeae
Noyan <noyannowhasyt@gmail.com>
2025-11-14 21:04:07 +0300

    Update RecordRules.js

--- js/components/Sidebar/RecordRules.js
+++ js/components/Sidebar/RecordRules.js
@@ -3,7 +3,7 @@ export default {
         <h3>Record Submission Requirements</h3>
         <div class="right-text">
             <p>
-                You must have achieved the record without using hacks (including hacks that change the physics of the game, ie. physics bypass via MegaHack. Note that Click Between Frames is allowed but its Physics Bypass setting is banned.).
+                You must have achieved the record without using hacks (including hacks that change the physics of the game, ie. physics bypass via MegaHack. Click Between Frames, either physics bypass is on or not is allowed.).
             </p>
             <p>
                 You must have achieved the record on the level that is listed on the site or on an approved bugfixed copy - please check the level ID before you submit a record!
